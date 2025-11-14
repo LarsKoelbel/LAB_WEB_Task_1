d190fd28ca0312ddd7630a2d947afdc13c87e636
LarsKoelbel <136699088+LarsKoelbel@users.noreply.github.com>
2025-11-14 16:08:48 +0100

    Add dynamic user info and leaderboard ranking
    Added display of user name and balance placeholders to headers in chart, leaderboard, and messages pages. Refactored leaderboard to dynamically fetch and render player rankings using a template and API data. Minor adjustment to SERVER_BASE_URL in lib.js.

--- public/javascripts/leaderboard.js
+++ public/javascripts/leaderboard.js
@@ -3,8 +3,38 @@
 import {
     initNavigationBar,
     updateNavigationBar,
-    DYNAMIC_UI_UPDATE_INTERVAL_IN_MS
+    DYNAMIC_UI_UPDATE_INTERVAL_IN_MS, getUser, getAllUsers
 } from "./lib.js";
+
+// Update the player ranking
+async function updatePlayerRanking()
+{
+    // Get all players
+    const users = await getAllUsers();
+
+    users.sort((a, b) => b.balance - a.balance);
+
+    // Get the list environment
+    const env = document.getElementById("tr-environment-dynamic-ranking-list");
+
+    // Grep the template
+    const template = document.getElementById("tr-environment-dynamic-ranking-list__element-row-template");
+
+    env.innerHTML = ``;
+    // Create one new element per user
+    let i = 1;
+    users.forEach(user => {
+        // Instance a new row template
+        const clone = template.content.cloneNode(true);
+
+        clone.querySelector(".tr-environment-dynamic-ranking-list__element-row-template__id").textContent = i;
+        clone.querySelector(".tr-environment-dynamic-ranking-list__element-row-template__username").textContent = user.name;
+        clone.querySelector(".tr-environment-dynamic-ranking-list__element-row-template__value").textContent = user.balance.toFixed(2) + " €";
+
+        env.appendChild(clone);
+        i++;
+    })
+}
 
 async function init ()
 {
@@ -16,6 +46,7 @@ async function init ()
 async function contentLoop()
 {
     updateNavigationBar();
+    updatePlayerRanking();
 }
 
 async function startContentLoop()
